ed96fd613d9119fc2928cdf5895386c961f4af5b
Stjepan Labor <stjepan.labor24@gmail.com>
2025-04-04 22:04:57 +0200

    spa-homework

--- Task01/Source.cpp
+++ Task01/Source.cpp
@@ -1,11 +1,12 @@
 #include <SFML/Graphics.hpp>
-
+#include"Cvijet.h"
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML works!");
+    window.setFramerateLimit(60);
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+    Cvijet cvijet(&window);
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -15,7 +16,7 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        cvijet.draw();
         window.display();
     }
 }
